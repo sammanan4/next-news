ef3133edcf21958c576a64372295ef6986e09f7a
Sheikh Abdul Manan <sammanan4@gmail.com>
2022-06-24 23:54:00 +0530

    fix express app

--- backend/app.js
+++ backend/app.js
@@ -9,19 +9,19 @@ app.get("/", (req, res) => {
   if (data) {
     console.log("received request for data ...");
     res.status(200).json(data);
-  } else{
-      res.status(500).send("Error");
+  } else {
+    res.status(500).send("Error");
   }
 });
 
 app.get("/:id", (req, res) => {
   const id = req.params.id;
-  console.log(id)
+  console.log(id);
   if (data.length > id) {
     const item = data[id];
-    res.status(200).json(item).end();
-  } else{
-      res.status(404).send("Error");
+    res.status(200).json(item);
+  } else {
+    res.status(404).send("Error");
   }
 });
 
